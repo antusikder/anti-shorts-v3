889b145d15801eb17c7d5471c3d00f670d7fbf4f
Antu <antualmasikder31416@penguin.linux>
2026-03-27 21:59:56 +0600

    Fresh Mind v3.1.6: Definitive SDK 54 Reconstruction & Native Stabilization

--- android/app/src/main/java/com/antishorts/shield/MainApplication.kt
+++ android/app/src/main/java/com/antishorts/shield/MainApplication.kt
@@ -16,8 +16,6 @@ import com.facebook.react.defaults.DefaultReactNativeHost
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
-
-
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
